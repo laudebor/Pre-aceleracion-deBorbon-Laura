5bd424360cdfc3421dd359148d09eaa776faab28
Laura de Borbón <laudebor@gmail.com>
2022-08-02 18:42:19 -0300

    Entity creation

---
+++ src/main/java/com/alkemy/disney/disney/entity/GeneroEntity.java
@@ -0,0 +1,23 @@
+package com.alkemy.disney.disney.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Tables;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "genero")
+@Getter
+@Setter
+public class GeneroEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nombre;
+
+    private String imagen;
+}
