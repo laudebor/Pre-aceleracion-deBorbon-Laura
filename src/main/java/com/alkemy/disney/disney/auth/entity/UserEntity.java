256d2e4efbfd9c9bb439b303106d11723e476eb5
Laura de Borbón <laudebor@gmail.com>
2022-08-16 18:27:30 -0300

    Security configuration

---
+++ src/main/java/com/alkemy/disney/disney/auth/entity/UserEntity.java
@@ -0,0 +1,87 @@
+package com.alkemy.disney.disney.auth.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user")
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Email
+    private String username;
+
+    @Size(min = 8)
+    private String password;
+
+    private boolean accountNotExpired;
+    private boolean accountNotLocked;
+    private boolean credentialsNotExpired;
+    private boolean enabled;
+
+    public UserEntity() {
+        this.accountNotExpired = true;
+        this.accountNotLocked = true;
+        this.credentialsNotExpired = true;
+        this.enabled = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNotExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNotLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNotExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
