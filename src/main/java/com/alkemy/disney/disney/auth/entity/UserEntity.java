b25c184223d210c954a02cd21999456e1c6cc3d1
Laura de Borbón <laudebor@gmail.com>
2022-08-19 12:17:21 -0300

    Changes of DB and IdUser

--- src/main/java/com/alkemy/disney/disney/auth/entity/UserEntity.java
+++ src/main/java/com/alkemy/disney/disney/auth/entity/UserEntity.java
@@ -13,7 +13,7 @@ import java.util.Collection;
 public class UserEntity implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Email
